cb50967126d9ad4f1b995572f30cb8d26295aecb
Jen Davis <jenptp@gmail.com>
2019-11-01 11:55:45 -0700

    taking out word generator to meet deadline. will add after presentation

--- word.js
+++ word.js
@@ -1,12 +1,12 @@
-/*This is the word generator for index.html*/
-var words = ["pineapple", "pizza", "laundry", "Italy", "puppies", "portable", "defense", "salon", "outlaw", "pottery", "chivalry", "surprise", "chemistry", "coder", "sunburn", "Bali", "princess", "artist", "pork chop", "revenge", "hotel", "kaleidoscope", "attack", "gremlin", "vampire", "rain", "author", "remix", "diva", "pudding", "NASA", "moon rocks", "sandwich", "elephant", "elevator", "fork", "forgery", "sister", "agent", "eclipse", "fall", "disguise", "unlucky", "health", "shark", "Detroit", "shark", "practical", "fantasy",];
+// /*This is the word generator for index.html*/
+// var words = ["pineapple", "pizza", "laundry", "Italy", "puppies", "portable", "defense", "salon", "outlaw", "pottery", "chivalry", "surprise", "chemistry", "coder", "sunburn", "Bali", "princess", "artist", "pork chop", "revenge", "hotel", "kaleidoscope", "attack", "gremlin", "vampire", "rain", "author", "remix", "diva", "pudding", "NASA", "moon rocks", "sandwich", "elephant", "elevator", "fork", "forgery", "sister", "agent", "eclipse", "fall", "disguise", "unlucky", "health", "shark", "Detroit", "shark", "practical", "fantasy",];
 
-var getRandomWord = function () {
-    return words[Math.floor(Math.random() * words.length)];
-};
+// var getRandomWord = function () {
+//     return words[Math.floor(Math.random() * words.length)];
+// };
 
-var word = getRandomWord();
+// var word = getRandomWord();
 
-console.log(word);
+// console.log(word);
 
-document.getElementById("word").textContent = word;
+// document.getElementById("word").textContent = word;
